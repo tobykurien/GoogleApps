09cb219f965da134487869f664047e35186d0c5d
tobykurien <tobykurien@192.168.0.25>
2013-11-14 11:24:29 +0200

    disabled unsupported preferences on older android versions

--- src/com/tobykurien/google_news/Preferences.java
+++ src/com/tobykurien/google_news/Preferences.java
@@ -1,5 +1,6 @@
 package com.tobykurien.google_news;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
@@ -8,6 +9,12 @@ public class Preferences extends PreferenceActivity {
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       addPreferencesFromResource(R.xml.settings);
+      
+      if (Build.VERSION.SDK_INT < 11) {
+         // older versions of Android don't support all settings
+         getPreferenceScreen().findPreference("block_3rd_party").setEnabled(false);
+         getPreferenceScreen().findPreference("hide_actionbar").setEnabled(false);
+      }
    }
    
    @Override
